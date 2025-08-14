6e62c63f873908da1716cab1336d50e12a5efc72
K-SindhuRani <sindhurani334@gmail.com>
2025-08-14 22:07:18 +0530

    Time: 14 ms (5.51%), Space: 43.8 MB (12.23%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -18,51 +18,5 @@ class Solution {
             arr[ind++] = list.get(i);
         }
         return arr;
-        /*for(int i=0;i<=nums1.length-1;i++){
-            if(!arr.contains(nums1[i])){
-                arr.add(nums1[i]);
-            }
-         }
-         ArrayList<Integer> arr1 = new ArrayList<Integer>();
-          for(int i=0;i<=nums2.length-1;i++){
-            if(arr.contains(nums2[i]) && !arr1.contains(nums2[i])){
-                arr1.add(nums2[i]);
-            }
-        }
-        int[] result = new int[arr1.size()];
-        int index = 0;
-        for(int i:result){
-            result[index++]=i;
-        }
-       HashMap<Integer,Integer> map1 = new HashMap<>();
-       HashMap<Integer,Integer> map2 = new HashMap<>();
-       for(int i=0;i<nums1.length;i++){
-        map1.put(nums1[i],map1.getOrDefault(nums1[i],0)+1);
-       }
-         for(int i=0;i<nums2.length;i++){
-        map2.put(nums2[i],map2.getOrDefault(nums2[i],0)+1);
-       }
-        for(int key : map1.keySet()){
-            if(map2.containsKey(key)){
-                if(map1.get(key).equals(map2.get(key))){
-                    for(int i=0;i<map1.get(key);i++){
-                        list.add(key);
-                    }
-                }
-            }
-        }
-       /* for(int i=0;i<nums1.length;i++){
-            for(int j=0;j<nums2.length;j++){
-                if(nums1[i]==nums2[j]){
-                    list.add(nums1[i]);
-                    break;
-                }
-            }
-        }
-          int[] result = new int[list.size()];
-          for(int i=0;i<result.length;i++){
-            result[i]= list.get(i);
-          }
-        return result;*/
     }
 }
